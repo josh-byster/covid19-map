d364ba94911cd6301618e1fa33176ed1d606a2b1
Josh Byster <joshb1050@gmail.com>
2020-03-17 05:03:55 -0500

    Added webpack

---
+++ d3/src/index.js
@@ -0,0 +1,18 @@
+// Test import of a JavaScript function, an SVG, and Sass
+import Map from './js/map'
+import Slider from './js/slider'
+import WebpackLogo from './images/webpack-logo.svg'
+import './styles/main.css'
+import './styles/slider.css'
+
+// // Create SVG logo node
+// const logo = document.createElement('img')
+// logo.src = WebpackLogo
+
+// // Create heading node
+// const greeting = document.createElement('h1')
+// greeting.textContent = HelloWorld()
+
+// // Append SVG and heading nodes to the DOM
+// const app = document.querySelector('#root')
+// app.append(logo, greeting)
