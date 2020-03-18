e3b97bcd41cdc1d6510798a18e1d4d48fca4fa08
Josh Byster <joshb1050@gmail.com>
2020-03-17 21:29:46 -0500

    Responsive slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,12 +4,21 @@ class Slider {
   formatSliderDate = d3.timeFormat("%b %-d");
   parseDate = d3.timeParse("%m/%d/%y");
 
-  startDate = d3.timeParse("%Y-%m-%d")("2020-01-23");
-  endDate = d3.timeParse("%Y-%m-%d")("2020-03-16");
+  startDate = d3.timeParse("%Y-%m-%d")("2020-01-22");
+  endDate = d3.timeParse("%Y-%m-%d")("2020-03-17");
+
+  computeDimensions = () => {
+    return {
+      width:
+        Math.min(500, window.innerWidth) - this.margin.left - this.margin.right,
+      height: 200 - this.margin.top - this.margin.bottom
+    };
+  };
 
   margin = { top: 0, right: 50, bottom: 0, left: 50 };
-  sliderWidth = 500 - this.margin.left - this.margin.right;
-  sliderHeight = 200 - this.margin.top - this.margin.bottom;
+  sliderWidth = this.computeDimensions().width;
+
+  sliderHeight = this.computeDimensions().height;
 
   sliderSvg = d3
     .select("#vis")
@@ -22,6 +31,7 @@ class Slider {
   targetValue = this.sliderWidth;
   map;
   playButton = d3.select("#play-button");
+  getNumTicks = () => (this.computeDimensions().width > 300 ? 4 : 2);
 
   x = d3
     .scaleTime()
@@ -34,7 +44,62 @@ class Slider {
     .attr("class", "slider")
     .attr("transform", "translate(" + this.margin.left + "," + 0 / 5 + ")");
 
+  // Slider responsiveness
+  resize = () => {
+    this.sliderSvg.attr(
+      "width",
+      this.computeDimensions().width + this.margin.left + this.margin.right
+    );
+    this.sliderSvg.attr(
+      "height",
+      this.computeDimensions().height + this.margin.top + this.margin.bottom
+    );
+    const oldX = d3
+      .scaleTime()
+      .domain([this.startDate, this.endDate])
+      .range(this.x.range())
+      .clamp(true);
+
+    this.x.range([0, this.computeDimensions().width]);
+    d3.select(".track")
+      .attr("x1", this.x.range()[0])
+      .attr("x2", this.x.range()[1]);
+    d3.select(".track-inset")
+      .attr("x1", this.x.range()[0])
+      .attr("x2", this.x.range()[1]);
+    d3.select(".track-overlay")
+      .attr("x1", this.x.range()[0])
+      .attr("x2", this.x.range()[1]);
+    const t = d3
+      .select(".ticks")
+      .selectAll("text")
+      .data(this.x.ticks(this.getNumTicks()));
+
+    t.enter()
+      .append("text")
+      .merge(t)
+      .attr("x", this.x)
+      .attr("y", 10)
+      .attr("text-anchor", "middle")
+      .text(d => {
+        return this.formatBottomDate(d);
+      });
+
+    t.exit().remove();
+
+    const convertToNew = t => this.x(oldX.invert(t));
+
+    const currentHandlePos = this.handle.attr("cx");
+    const newHandlePos = convertToNew(currentHandlePos);
+    const currentLabelPos = this.label.attr("x");
+    this.handle.attr("cx", newHandlePos);
+    this.label
+      .attr("x", convertToNew(currentLabelPos))
+      .text(this.formatSliderDate(this.x.invert(newHandlePos)));
+  };
+
   constructor(map) {
+    window.addEventListener("resize", this.resize);
     this.map = map;
     const self = this;
     this.slider
@@ -67,7 +132,7 @@ class Slider {
       .attr("class", "ticks")
       .attr("transform", "translate(0," + 18 + ")")
       .selectAll("text")
-      .data(this.x.ticks(4))
+      .data(this.x.ticks(this.getNumTicks()))
       .enter()
       .append("text")
       .attr("x", this.x)
@@ -76,10 +141,12 @@ class Slider {
       .text(d => {
         return this.formatBottomDate(d);
       });
+
     this.handle = this.slider
       .insert("circle", ".track-overlay")
       .attr("class", "handle")
       .attr("r", 15)
+      .attr("cx", 0)
       .call(x => {});
 
     this.label = this.slider
@@ -87,7 +154,8 @@ class Slider {
       .attr("class", "label")
       .attr("text-anchor", "middle")
       .text(this.formatSliderDate(this.startDate))
-      .attr("transform", "translate(0," + -25 + ")");
+      .attr("transform", "translate(0," + -25 + ")")
+      .attr("x", 0);
   }
 
   update(h) {
